cb2f0424f4030ca797c06ac71b359493c3c30c87
Marie <mariezarrini@gmail.com>
2022-08-12 23:03:51 +0430

    Add Factories.

---
+++ OrderSystem.Tests.Unit/Factories/OrderFactory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace OrderSystem.Tests.Unit.Factories
+{
+	public class OrderFactory
+	{
+		private readonly int _userId;
+		private List<OrderItem> _orderItems = new();
+		OrderItem orderItem = new OrderItemFactory().GetSomeOrderItem();
+
+		public OrderFactory()
+		{
+			_userId = 1;
+			_orderItems.Add(orderItem);
+		}
+
+
+		public Order GetSomeOrder()
+		{
+			return new Order(_userId, _orderItems);
+		}
+
+		public Order GetSomeOrder(int userId)
+		{
+			return new Order(userId, _orderItems);
+		}
+
+		public Order GetSomeOrder(OrderItem orderItem)
+		{
+			_orderItems = new List<OrderItem>() { orderItem };
+			return new Order(_userId, _orderItems);
+		}
+
+		public Order GetSomeOrder(List<OrderItem> orderItems)
+		{
+			return new Order(_userId, orderItems);
+		}
+
+		public Order GetSomeOrder(int userId, OrderItem orderItem)
+		{
+			_orderItems = new List<OrderItem>() { orderItem };
+			return new Order(userId, _orderItems);
+		}
+	}
+}
---
+++ OrderSystem.Tests.Unit/Factories/OrderItemFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace OrderSystem.Tests.Unit.Factories
+{
+	public class OrderItemFactory
+	{
+		private readonly int _count;
+		private readonly string _name;
+
+		public OrderItemFactory()
+		{
+			_count = 2;
+			_name = "laptop";
+		}
+
+
+		public OrderItem GetSomeOrderItem()
+		{
+			return new OrderItem(_count, _name);
+		}
+
+		public OrderItem GetSomeOrderItem(int count)
+		{
+			return new OrderItem(count, _name);
+		}
+
+		public OrderItem GetSomeOrderItem(string name)
+		{
+			return new OrderItem(_count, name);
+		}
+
+		public OrderItem GetSomeOrderItem(int count, string name)
+		{
+			return new OrderItem(count, name);
+		}
+	}
+}
